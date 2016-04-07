d1a59e6a3a9837c3a00b8e5af886b904210935df
Derek Jass <jazzer36@gmail.com>
2016-04-07 09:59:19 -0500

    Finished food/drink/chem data

--- validate.py
+++ validate.py
@@ -6,45 +6,40 @@ with open("_data/items.json") as f:
     for item in json.load(f, object_hook=examine.AttrDict):
         items[item.id] = item
 
-def get_by_id(id):
-    return items[id]
+def where_used(productid):
+    item = items[productid]
+    return [i.id for i in item.used_in] if 'used_in' in item else []
 
-def listed_as_ingredient(reagent, product):
-    for component in product.components:
-        if component.id == reagent.id:
-            return True
-    return False
+def components(productid):
+    item = items[productid]
+    return [i.id for i in item.components] if 'components' in item else []
 
-def listed_as_product(product, reagent):
-    for recipe in reagent.used_in:
-        if recipe.id == product.id:
-            return True
-    return False
+missingids, missingcomponents, missingrecipes = [], [], []
 
 for item in items.itervalues():
-    if 'components' in item:
-        for component in item.components:
-            if not component.id in items:
-                error = "{}\n\tNo item with id: {}"
-                print error.format(item.name, component.id)
-                continue
-            if not 'used_in' in items[component.id]:
-                print "warn" + item.name
-                continue
-            for whereused in items[component.id].used_in:
-                if whereused.id == item.id:
-                    break
-            if not listed_as_product(item, items[component.id]):
-                error = "{} does not list {} as a recipe it's part of."
-                print error.format(items[component.id].name, item.name)
-    if 'used_in' in item:
-        for usedin in item.used_in:
-            if not usedin.id in items:
-                error = "{}\n\tNo item with id: {}"
-                print error.format(item.name, usedin.id)
-                continue
-            if not listed_as_ingredient(item, items[usedin.id]):
-                error = "{} does not list {} as a component."
-                print error.format(items[usedin.id].name, item.name)
-            
+    for recipeid in where_used(item.id):
+        if not recipeid in items:
+            errmsg = "No item with id: {}. {} is a component."
+            missingids.append(errmsg.format(recipeid, item.name))
+            continue
+        if not 'effect' in items[recipeid]:
+            continue
+        if not item.id in components(recipeid):
+            errmsg = "{} does not list {} as a component"
+            missingcomponents.append(errmsg.format(
+                items[recipeid].name, item.name))
+    for componentid in components(item.id):
+        if not componentid in items:
+            errmsg = "No item with id: {}. Used in {}."
+            missingids.append(errmsg.format(componentid, item.name))
+            continue
+        if not 'effect' in items[componentid]:
+            continue
+        if not item.id in where_used(componentid):
+            errmsg = "{} does not show {} as a recipe it's used in."
+            missingrecipes.append(errmsg.format(
+                items[componentid].name, item.name))
 
+print '\n'.join(missingids)
+print '\n'.join(missingcomponents)
+print '\n'.join(missingrecipes)
